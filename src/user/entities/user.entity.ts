fbe4bacc50963492f35d62e565b0178714a81bff
fernandoG494 <darkogarcia3@gmail.com>
2024-04-27 17:03:45 -0600

    Schema added

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class User {
+  @Prop({ unique: true })
+  _id: string;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ unique: true, required: true })
+  email: string;
+
+  @Prop({ minlength: 6, required: true })
+  password: string;
+
+  @Prop({ default: true })
+  isActive: boolean;
+
+  @Prop({ type: [String], default: ['user'] })
+  roles: string[];
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
